2e6362e6d5b8e5f0a6937de2ff83964dec6bb337
Saurabh Nanda <saurabh@vacationlabs.com>
2017-11-10 13:03:14 +0530

    Fixed stupid mistakes

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
@@ -8,23 +9,28 @@ import GHC.Stack
 import Debug.Trace
 import Control.Exception
 import Control.DeepSeq
+import GHC.Generics
+
+data CustomException = CustomException String CallStack deriving (Show, Generic)
+instance Exception CustomException
+instance NFData CustomException
 
 -- TODO: Is this the correct way to implement an NFData for SomeException?
-instance NFData SomeException where
-  rnf e = rnf (show e)
+-- instance NFData SomeException where
+--   rnf e = rnf (show e)
 
 factorialCS :: (HasCallStack) => Integer -> Integer
 factorialCS 1 = 1
-factorialCS 3 = error "Error from CS version"
+factorialCS 3 = throw $ CustomException "Error from CS version" callStack
 factorialCS n = n * factorialCS (n - 1)
 
 factorial :: Integer -> Integer
 factorial 1 = 1
-factorial 3 = error "Error from non-CS version"
-factorial n = n * factorialCS (n - 1)
+factorial 3 = throw $ CustomException "Error from non-CS version" emptyCallStack
+factorial n = n * factorial (n - 1)
 
 catchExceptions :: (Integer -> Integer) -> IO ()
-catchExceptions fn = catch runFn $ \ ((!e) :: SomeException) -> e `deepseq` pure ()
+catchExceptions fn = catch runFn $ \ ((!e) :: CustomException) -> e `deepseq` pure ()
   where
     runFn = do
       let x = fn 20
