f055b16db7bf42e069daf25e126bd04a3c8b65bc
Saurabh Nanda <saurabh@vacationlabs.com>
2017-11-10 13:14:03 +0530

    Comparing for callstack depth

--- app/Main.hs
+++ app/Main.hs
@@ -29,11 +29,11 @@ factorial 1 = 1
 factorial 3 = throw $ CustomException "Error from non-CS version" emptyCallStack
 factorial n = n * factorial (n - 1)
 
-catchExceptions :: (Integer -> Integer) -> IO ()
-catchExceptions fn = catch runFn $ \ ((!e) :: CustomException) -> e `deepseq` pure ()
+catchExceptions :: (Integer -> Integer) -> Integer -> IO ()
+catchExceptions fn n = catch runFn $ \ ((!e) :: CustomException) -> e `deepseq` pure ()
   where
     runFn = do
-      let x = fn 20
+      let x = fn n
       traceIO (show x)
 
 
@@ -42,8 +42,18 @@ main :: IO ()
 main = defaultMain
   [
     bgroup "factorial"
-    [ bench "regular" $ nfIO $ catchExceptions factorial
-    , bench "callstack" $ nfIO $ catchExceptions factorialCS
+    [ bench "regular/20" $ nfIO $ catchExceptions factorial 20
+    , bench "callstack/20" $ nfIO $ catchExceptions factorialCS 20
+    , bench "regular/30" $ nfIO $ catchExceptions factorial 30
+    , bench "callstack/30" $ nfIO $ catchExceptions factorialCS 30
+    , bench "regular/50" $ nfIO $ catchExceptions factorial 50
+    , bench "callstack/50" $ nfIO $ catchExceptions factorialCS 50
+    , bench "regular/100" $ nfIO $ catchExceptions factorial 100
+    , bench "callstack/100" $ nfIO $ catchExceptions factorialCS 100
+    , bench "regular/200" $ nfIO $ catchExceptions factorial 200
+    , bench "callstack/200" $ nfIO $ catchExceptions factorialCS 200
+    , bench "regular/300" $ nfIO $ catchExceptions factorial 300
+    , bench "callstack/300" $ nfIO $ catchExceptions factorialCS 300
     ]
   ]
 
